6682e3d259b6161cdf450a39ac9429ce1d02b7be
Duggu-Jhansi <duggujhansireddy123@gmail.com>
2024-02-27 18:15:13 +0530

    commit message

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <div class="bg">
+     <h1>Plan Wherever You want to go</h1>
+     <div>
+     <button>Get started</button>
+    </div>
+    </div>
+    </main>
+</template>
+<script setup>
+</script>
+<style scoped>
+.bg{
+    background-image: url('https://i.ytimg.com/vi/nZSC3Zpe-Qw/maxresdefault.jpg');
+    background-size:cover;
+    height:100vh;
+}
+div{
+    
+  
+    text-align: center;
+}
+button{
+    border:0px;
+    border-radius: 10px;
+    border-width:10px;
+}
+</style>
